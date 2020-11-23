f879bd16509138e8d27b5524922087522120ef3c
Rihito Iijima <iijima@onefinancial.jp>
2020-11-24 06:07:31 +0900

    abc 184 e

--- atcoder_beginner_contests/184/e.py
+++ atcoder_beginner_contests/184/e.py
@@ -1,119 +1,92 @@
-from dataclasses import dataclass
-from itertools import combinations
-from typing import List
+from typing import List, Set, NewType, Tuple
 
 WALL = '#'
+START = 'S'
+GOAL = 'G'
 
-@dataclass(frozen=True)
-class Position:
-    h: int
-    w: int
-    s: str
-
-    def is_wall(self) -> bool:
-        return self.s == WALL
-
-@dataclass(frozen=True)
-class Edge:
-    p1: Position
-    p2: Position
-
-@dataclass(frozen=True)
-class EdgeList:
-    l: List[Edge]
-
-    def add(self, edge: Edge) -> None:
-        self.l.append(edge)
+Y, X, CHAR = 0, 1, 2
+position = NewType('Position', Tuple[int, int, str])
 
 class Map:
-    def __init__(self, H: int, W: int, a: List[List[str]]) -> None:
+    _A = 97
+    _Z = 122
+    _TELEPORTS: Set[str] = {chr(i) for i in range(_A, _Z+1)}
+
+    def __init__(self, H: int, W: int, a: List[List[str]], debug=False) -> None:
         self.H = H
         self.W = W
         self.a = a
-        self.positions: List[List[Position]] = [[Position(h, w, a[h][w]) for w in range(W)] for h in range(H)]
-        self.edges: EdgeList = EdgeList([])
-        # a = 97 ~ z = 122
-        for i in range(97, 123):
-            positions = self.get_positions_by_str(chr(i))
-            for combi in combinations(positions, 2):
-                self.edges.add(Edge(combi[0], combi[1]))
+        self.debug = debug
 
-        # 隣会う Position をエッジでつなぐ
-        for h in range(H):
-            for w in range(W):
-                me = self.positions[h][w]
-                if me.is_wall():
-                    continue
+        above, below, left, right = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        connection = [above, below, left, right]
+        self.__teleports = {c: [] for c in self._TELEPORTS}
+        def process(h, w, char) -> List[position]:
+            if char in self._TELEPORTS:
+                self.__teleports[char].append(position((h, w, char)))
 
-                if h != H - 1:
-                    below = self.positions[h+1][w]
-                    if not below.is_wall():
-                        self.edges.add(Edge(me, below))
+            return [
+                    position((h+c[Y], w+c[X], a[h+c[Y]][w+c[X]]))
+                    for c in connection
+                    if (h != 0 or c != above) and \
+                        (h != H-1 or c != below) and \
+                        (w != 0 or c != left) and \
+                        (w != W-1 or c != right) and \
+                        char != WALL and \
+                        a[h+c[Y]][w+c[X]] != WALL
+            ]
 
-                if w != W - 1:
-                    right = self.positions[h][w+1]
-                    if not right.is_wall():
-                        self.edges.add(Edge(me, right))
-
-        self.__connected_positions: List[List[List[Position]]] = [
+        self.__connected_positions: List[List[List[position]]] = [
             [
-                list(map(
-                    lambda e: e.p1 if e.p2 == self.positions[h][w] else e.p2,
-                    filter(
-                        lambda e: e.p1 == self.positions[h][w] or e.p2 == self.positions[h][w],
-                        self.edges.l
-                    )
-                ))
-                for w in range(W)
-            ] for h in range(H)
+                process(h, w, char)
+                for w, char in enumerate(row)
+            ] for h, row in enumerate(a)
         ]
 
-    def get_positions_by_str(self, s: str) -> List[Position]:
-        positions: List[Position] = []
-        for row in self.positions:
-            for pos in row:
-                if pos.s == s:
-                    positions.append(pos)
+    def start(self) -> position:
+        for h in range(self.H):
+            for w in range(self.W):
+                if self.a[h][w] == START:
+                    return position((h, w, START))
 
-        return positions
+    def goal(self) -> position:
+        for h in range(self.H):
+            for w in range(self.W):
+                if self.a[h][w] == GOAL:
+                    return position((h, w, GOAL))
 
-    def start(self) -> Position:
-        return self.get_positions_by_str('S')[0]
-
-    def goal(self) -> Position:
-        return self.get_positions_by_str('G')[0]
-
-    def distance(self, start: Position, goal: Position) -> int:
-        distances = [[None for w in range(self.W)] for h in range(self.H)]
-
+    def distance(self, start: position, goal: position) -> int:
         distance = 0
         queues = [start]
+        visited = set()
         # 幅優先探索
         while len(queues) > 0:
-            self.print_map(queues)
+            if self.debug:
+                self.print_map(queues)
+
             queue_length = len(queues)
             for i in range(queue_length):
                 pos = queues[i]
                 if pos == goal:
                     return distance
 
-                if distances[pos.h][pos.w] is None:
-                    distances[pos.h][pos.w] = distance
-                else:
-                    continue
-
-                connecteds = self.__connected_positions[pos.h][pos.w]
+                visited.add(pos)
+                connecteds = self.__connected_positions[pos[Y]][pos[X]]
                 queues.extend(connecteds)
-            queues = queues[queue_length:]
+                if pos[CHAR] in self._TELEPORTS:
+                    teleports = self.__teleports[pos[CHAR]]
+                    queues.extend(teleports)
+
+            queues = [q for q in queues[queue_length:] if q not in visited]
             distance += 1
         else:
             return -1
 
-    def print_map(self, positions: List[Position]) -> None:
+    def print_map(self, positions: List[position]) -> None:
         print(''.join('-' for _ in range(self.W*2+7)))
 
         for h in range(self.H):
-            ps = list(map(lambda p: p.w, filter(lambda p: p.h == h, positions)))
+            ps = list(map(lambda p: p[X], filter(lambda p: p[Y] == h, positions)))
             row = ['X' if i in ps else '·' for i in range(self.W)]
             print('| ', end='')
             print(''.join(row), end='')
@@ -123,31 +96,30 @@ class Map:
 
         print(''.join('-' for _ in range(self.W*2+7)))
 
-def solve(H: int, W: int, a: List[List[str]]) -> int:
-    map = Map(H, W, a)
+def solve(H: int, W: int, a: List[List[str]], debug=False) -> int:
+    map = Map(H, W, a, debug)
     start = map.start()
     goal = map.goal()
     return map.distance(start, goal)
 
+ss = """Sa...............................#...#...#...#....
+#############......a.a..b..#...#z..#...#...#......
+a........#..#...#...#...#....##...#...#.b.#....G.#"""
+
+def test():
+    a = list(map(lambda row: list(row), ss.split('\n')))
+    H = len(a)
+    W = len(a[0])
+
+    print(solve(H, W, a, debug=True))
+
 def main():
-    # H, W = map(int, input().split())
-    # a = [list(input()) for _ in range(H)]
-    H, W = (11, 11)
-    a = [
-        list('.#.#.e#a...'),
-        list('.b..##..#..'),
-        list('#....#.#..#'),
-        list('.#dd..#..#.'),
-        list('....#...#e.'),
-        list('c#.#a....#.'),
-        list('.....#..#.e'),
-        list('.#....#b.#.'),
-        list('.#...#..#..'),
-        list('......#c#G.'),
-        list('#..S...#...'),
-    ]
+    H, W = map(int, input().split())
+    a = [list(input()) for _ in range(H)]
 
     print(solve(H, W, a))
 
 if __name__ == '__main__':
-    main()
+    test()
+    # main()
+
